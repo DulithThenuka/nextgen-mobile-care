ebf5595bbe0830055be0e20929ad31f986f872d9
Dulith Thenuka <duliththenuka912@gmail.com>
2026-02-26 10:43:25 +0530

    create footer.php

---
+++ app/views/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-900 text-center py-6 mt-20">
+    <p class="text-gray-400">
+        © <?php echo date('Y'); ?> NextGen Mobile Care. All rights reserved.
+    </p>
+</footer>
+
+</body>
+</html>
